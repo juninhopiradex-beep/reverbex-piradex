db4f9e5a0aeb1b496dfe603be2d7a153ae26a2f9
juninhopiradex-beep <juninhopiradex@gmail.com>
2026-05-31 18:37:17 +0100

    Update PluginProcessor.h

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
 
 class ReverbAudioProcessor : public juce::AudioProcessor,
                              public juce::AudioProcessorValueTreeState::Listener
