14c4aa4e81cb9613b89857a5a3091d047fe15a7b
juninhopiradex-beep <juninhopiradex@gmail.com>
2026-05-31 18:10:53 +0100

    Create PluginProcessor.h

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <JuceHeader.h>
+
+class ReverbAudioProcessor : public juce::AudioProcessor,
+                             public juce::AudioProcessorValueTreeState::Listener
+{
+public:
+    ReverbAudioProcessor();
+    ~ReverbAudioProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return JucePlugin_Name; }
+    bool acceptsMidi()  const override { return false; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 2.0; }
+
+    int  getNumPrograms()    override { return 1; }
+    int  getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override {}
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override {}
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    juce::AudioProcessorValueTreeState apvts;
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    void parameterChanged (const juce::String& parameterID, float newValue) override;
+
+private:
+    juce::dsp::Reverb reverb;
+    juce::dsp::Reverb::Parameters reverbParams;
+    void updateReverbParams();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReverbAudioProcessor)
+};
